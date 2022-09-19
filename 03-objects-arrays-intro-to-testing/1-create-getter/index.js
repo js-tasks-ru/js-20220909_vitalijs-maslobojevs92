9a2c8d0630c67b41e832d65f8669310ef3cf4c62
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-09-19 18:00:56 +0300

    fixed 03-module 1st and 4th tasks

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,8 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const values = path.split('.');
+  
   return function(obj) {
-    const values = path.split('.');
     let result;
 
     for (const value of values) {
