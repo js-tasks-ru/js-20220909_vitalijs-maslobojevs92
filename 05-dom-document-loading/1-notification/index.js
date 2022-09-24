4f67a3482eb20969dbab1485b459d6940181929f
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-09-25 02:19:47 +0300

    solved 05-module 1-notifiction 1-task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,76 @@
 export default class NotificationMessage {
+  static rootElement;
+  static timer;
 
+  constructor(
+    message,
+    {
+      duration = 2000,
+      type = 'success'
+    } = {}
+  ) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.init();
+    this.render();
+  }
+
+  init() {
+    if (NotificationMessage.rootElement) {
+      clearTimeout(NotificationMessage.timer);
+      this.destroy();
+    }
+  }
+
+  render() {
+    const wrapper = document.createElement('DIV');
+    
+    wrapper.innerHTML = this.getTemplate();
+
+    NotificationMessage.rootElement = wrapper.firstElementChild;
+  }
+
+  getTemplate() {
+    const seconds = this.duration / 1000 + 's';
+
+    return `
+      <div class="notification ${this.type}" style="--value:${seconds}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.message}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  show(target) {
+    if (target) {
+      target.append(NotificationMessage.rootElement);
+    } else {
+      document.body.append(NotificationMessage.rootElement);
+    }
+
+    NotificationMessage.timer = setTimeout(this.destroy.bind(this), this.duration);
+  }
+
+  remove() {
+    NotificationMessage.rootElement.remove();
+  }
+
+  destroy() {
+    if (NotificationMessage.rootElement) {
+      this.remove();
+      NotificationMessage.rootElement = null;
+      NotificationMessage.timer = null;
+    }
+  }
+
+  get element() {
+    return NotificationMessage.rootElement;
+  }
 }
