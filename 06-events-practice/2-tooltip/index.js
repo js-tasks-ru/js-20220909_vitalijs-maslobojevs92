99e94e95b08c69aa76f798b93bd9db5237f16ee1
Vitalijs Maslobojevs <vitalijs.maslobojevs92@gmail.com>
2022-09-29 19:08:12 +0300

    solved 06-module 2-tooltip task

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,76 @@
 class Tooltip {
-  initialize () {
+  static instance = null;
 
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    return Tooltip.instance = this;
+  }
+
+  initialize () {
+    //нужно ли создавать отдельный метод, что бы можно было убить событие pointerover?
+    document.addEventListener('pointerover', e => {
+      if (e.target.closest('[data-tooltip]')) {
+
+        this.render(e.target.dataset.tooltip);
+
+        document.addEventListener('pointermove', this.onPointerMove);  
+        document.addEventListener('pointerout', this.onPointerOut);
+      }
+    });
+  }
+
+  onPointerOut = () => {
+    this.destroy();
+  }
+
+  onPointerMove = (e) => {
+    //При залезании тултипа за границу документа, тултип смещается на противоположную сторону курсора.
+    
+    let indentX = 10;
+    let indentY = 10;
+
+    let PosX = e.clientX;
+    let PosY = e.clientY;
+
+    if (document.documentElement.clientWidth < PosX + indentX + this.tooltip.clientWidth) {
+      PosX -= this.tooltip.clientWidth;
+      indentX = -indentX; 
+    }
+
+    if (document.documentElement.clientHeight < PosY + indentY + this.tooltip.clientHeight) {
+      PosY -= this.tooltip.clientHeight;
+      indentY = -indentY; 
+    }
+
+    this.tooltip.style.top = indentY + PosY + "px";
+    this.tooltip.style.left = indentX + PosX + "px";
+  
+  }
+
+  render(text) {
+    this.tooltip = document.createElement('div');
+    this.tooltip.classList.add('tooltip');
+    this.tooltip.innerHTML = text;
+
+    document.body.append(this.tooltip);
+  }
+
+
+  destroy() {
+    document.removeEventListener('pointermove', this.onPointerMove); 
+    document.removeEventListener('pointerout', this.onPointerOur);
+
+    if (this.tooltip) {
+      this.tooltip.remove();
+      this.tooltip = null;
+    } 
+  }
+
+  get element() {
+    return this.tooltip;
   }
 }
 
